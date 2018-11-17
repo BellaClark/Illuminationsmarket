e44f15e7edcea4dd610fd1010539b3f19945a4cd
Isabella Clark <bellaclark44@gmail.com>
2018-11-16 23:09:03 -0800

    plswokr2

--- illuminations/settings.py
+++ illuminations/settings.py
@@ -145,8 +145,8 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 import dj_database_url
 
-  DATABASES = {
-      'default': dj_database_url.config(
-          default='sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3'))
-      )
-  }
+DATABASES = {
+    'default': dj_database_url.config(
+        default='sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3'))
+    )
+}
